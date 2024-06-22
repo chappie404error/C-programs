2e1bab7b339ba5797af0cdc6439b356512bdc7af
Prathvirajmv <168354749+chappie404error@users.noreply.github.com>
2024-06-22 20:18:35 +0530

    Update arithmetic operators.c

--- arithmetic operators.c
+++ arithmetic operators.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 
 int main(){
-
     int a,b;
     printf("Enter 2 numbers");
     scanf("%d %d", &a,&b);
-
     printf("%d + %d=%d \n",a,b,a+b);
     printf("%d - %d=%d \n",a,b,a-b);
     printf("%d / %d=%d \n",a,b,a/b);
     printf("%d mod %d=%d \n",a,b,a%b);
     printf("%d * %d=%d \n ",a,b,a*b);
+
+    return 0;
 
 }
