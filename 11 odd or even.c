409f77c7daa168dce1d8c9991ef7495aded9b682
Prathvirajmv <168354749+chappie404error@users.noreply.github.com>
2024-06-23 12:51:30 +0530

    Create 11 odd or even.c

---
+++ 11 odd or even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+
+
+    int a;
+    printf("enter the number\n");
+    scanf("%d",&a);
+
+    ((a%2)==0)?printf("number %d is even",a):printf("number %d is odd",a);
+
+
+
+    
+}
