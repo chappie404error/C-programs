b6b5d0e9a1347ba5a06a8dc412432f8e5138fc2b
Prathvirajmv <168354749+chappie404error@users.noreply.github.com>
2024-06-23 12:35:43 +0530

    Create 7 sizeof.c

---
+++ 7 sizeof.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+void main(){
+     
+    int num;
+    float f;
+    char c;
+    double d;
+
+    printf("size of char is %ld byes \n",sizeof(c));
+    printf("size of int is %ld byes \n",sizeof(num));
+    printf("size of float is %ld byes \n",sizeof(f));
+    printf("size of double is %ld byes \n",sizeof(d));
+
+
+    
+    
+}
