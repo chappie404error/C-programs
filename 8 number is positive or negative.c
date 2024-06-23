d2cd057b2e60baf5672710c70e300c0eaa2d62b3
Prathvirajmv <168354749+chappie404error@users.noreply.github.com>
2024-06-23 12:41:09 +0530

    Create 8 number is positive or negative.c

---
+++ 8 number is positive or negative.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+
+void main(){
+     
+    int num;
+    printf("enter the number");
+    scanf("%d",&num);
+
+    num>0?printf("positive number\n"):printf("number is negative\n");
+
+
+    // using if 
+    if (num>0){
+        printf("number is postive");
+    }   
+    else{
+        printf("number is negative");
+    }
+}
